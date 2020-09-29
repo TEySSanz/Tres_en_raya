e963f45793b5c17e88eab17b47131ca857537f2c
David Sanz <dsanz@teys.eu>
2020-09-29 10:57:33 +0200

    Curso Android. Ejercicio práctico I. Vídeo 39

---
+++ app/src/main/java/eu/teys/dsanz/tres_en_raya/MainActivity.java
@@ -0,0 +1,14 @@
+package eu.teys.dsanz.tres_en_raya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
